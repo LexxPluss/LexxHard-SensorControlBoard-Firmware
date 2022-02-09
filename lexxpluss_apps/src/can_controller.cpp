3b4ee30ee65e218abb1077835c61ac1af431c92e
Atsunori Saito <sai@yedo.com>
2022-02-09 14:57:18 +0900

    Version 1.0.4

--- lexxpluss_apps/src/can_controller.cpp
+++ lexxpluss_apps/src/can_controller.cpp
@@ -324,7 +324,7 @@ private:
     const device *dev{nullptr};
     char version_powerboard[32] = "";
     bool heartbeat_timeout{true};
-    static constexpr char version[] = "1.0.3";
+    static constexpr char version[] = "1.0.4";
 } impl;
 
 int bmu_info(const shell *shell, size_t argc, char **argv)
