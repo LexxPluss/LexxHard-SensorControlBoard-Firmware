e4f49951e7cc653c4e89144840c21582d04843dc
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2023-09-14 11:48:10 +0900

    removed pull-up for the encoder input of center actuator

--- lexxpluss_apps/src/actuator_controller.cpp
+++ lexxpluss_apps/src/actuator_controller.cpp
@@ -96,7 +96,7 @@ extern "C" void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim_encoder)
         __HAL_RCC_GPIOD_CLK_ENABLE();
         GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_PULLUP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
         HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
