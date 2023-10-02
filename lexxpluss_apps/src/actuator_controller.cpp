5982671d658f793fa6e2399971da66235d66453d
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2023-10-03 08:41:39 +0900

    GPIO parameter changed(tested on Wani unit)

--- lexxpluss_apps/src/actuator_controller.cpp
+++ lexxpluss_apps/src/actuator_controller.cpp
@@ -77,8 +77,8 @@ extern "C" void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim_encoder)
         __HAL_RCC_TIM1_CLK_ENABLE();
         __HAL_RCC_GPIOE_CLK_ENABLE();
         GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_11;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-        GPIO_InitStruct.Pull = GPIO_PULLUP;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_OD; //GPIO_MODE_AF_OD
+        GPIO_InitStruct.Pull = GPIO_PULLUP; //GPIO_PULLUP
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
         HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
@@ -86,8 +86,8 @@ extern "C" void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef *htim_encoder)
         __HAL_RCC_TIM3_CLK_ENABLE();
         __HAL_RCC_GPIOC_CLK_ENABLE();
         GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP; //GPIO_MODE_AF_OD
-        GPIO_InitStruct.Pull = GPIO_NOPULL;     //GPIO_PULLUP
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_OD; //GPIO_MODE_AF_OD
+        GPIO_InitStruct.Pull = GPIO_PULLUP;     //GPIO_PULLUP  GPIO_NOPULL
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
         HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
