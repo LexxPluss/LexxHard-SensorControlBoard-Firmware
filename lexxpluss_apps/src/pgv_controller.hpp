47d38a442a0a149a38de8a6c7e375a4a302626f4
shujima <mss57kym@gmail.com>
2024-02-12 13:34:37 +0900

    changed the file name of can_pgv temporary to easy to review on Git Hub, and delete comment outed rows of ROS

--- lexxpluss_apps/src/pgv_controller.hpp
+++ lexxpluss_apps/src/pgv_controller.hpp
@@ -31,14 +31,6 @@ namespace lexxhard::pgv_controller {
 
 struct msg {
     uint8_t rawdata[21];
-    // uint32_t xp, tag;
-    // int32_t xps;
-    // int16_t yps;
-    // uint16_t ang, cc1, cc2, wrn;
-    // uint8_t addr, lane, o1, s1, o2, s2;
-    // struct {
-    //     bool cc2, cc1, wrn, np, err, tag, rp, nl, ll, rl;
-    // } f;
 } __attribute__((aligned(4)));
 
 struct msg_control {
--- lexxpluss_apps/src/rosserial.cpp
+++ lexxpluss_apps/src/rosserial.cpp
@@ -31,7 +31,6 @@
 #include "rosserial_imu.hpp"
 #include "rosserial_interlock.hpp"
 #include "rosserial_led.hpp"
-//#include "rosserial_pgv.hpp"
 #include "rosserial_tof.hpp"
 #include "rosserial_uss.hpp"
 #include "rosserial.hpp"
@@ -50,7 +49,6 @@ public:
         imu.init(nh);
         interlock.init(nh);
         led.init(nh);
-        //pgv.init(nh);
         tof.init(nh);
         uss.init(nh);
         return 0;
@@ -65,7 +63,6 @@ public:
             imu.poll();
             interlock.poll();
             led.poll();
-            //pgv.poll();
             tof.poll();
             uss.poll();
             k_usleep(1);
@@ -80,7 +77,6 @@ private:
     ros_imu imu;
     ros_interlock interlock;
     ros_led led;
-    //ros_pgv pgv;
     ros_tof tof;
     ros_uss uss;
 } impl;
