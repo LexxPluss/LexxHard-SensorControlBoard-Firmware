2a60466335ad868b9865c2787de44af22d129142
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2024-02-26 11:03:31 +0900

    added verification code for CAN1 and CAN2

--- lexxpluss_apps/src/main.cpp
+++ lexxpluss_apps/src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023, LexxPluss Inc.
+ * Copyright (c) 2022, LexxPluss Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,39 +23,15 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
-#include "actuator_controller.hpp"
-#include "adc_reader.hpp"
-#include "can_controller.hpp"
-#include "firmware_updater.hpp"
-#include "imu_controller.hpp"
-#include "interlock_controller.hpp"
-#include "led_controller.hpp"
-#include "misc_controller.hpp"
-#include "pgv_controller.hpp"
-#include "rosserial.hpp"
-#include "rosserial_service.hpp"
-#include "runaway_detector.hpp"
-#include "tof_controller.hpp"
-#include "uss_controller.hpp"
+#include <zephyr.h>
+#include <drivers/gpio.h>
+#include "can_dummy_v7.hpp"
+// #include "can_dummy_v7_2.hpp"
 
 namespace {
 
-K_THREAD_STACK_DEFINE(actuator_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(adc_reader_stack, 2048);
-K_THREAD_STACK_DEFINE(can_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(firmware_updater_stack, 2048);
-K_THREAD_STACK_DEFINE(imu_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(interlock_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(led_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(misc_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(pgv_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(rosserial_stack, 2048);
-K_THREAD_STACK_DEFINE(rosserial_service_stack, 2048);
-K_THREAD_STACK_DEFINE(runaway_detector_stack, 2048);
-K_THREAD_STACK_DEFINE(tof_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(uss_controller_stack, 2048);
+K_THREAD_STACK_DEFINE(can_dummy_v7_stack, 2048);
+// K_THREAD_STACK_DEFINE(can_dummy_v7_2_stack, 2048);
 
 #define RUN(name, prio) \
     k_thread_create(&lexxhard::name::thread, name##_stack, K_THREAD_STACK_SIZEOF(name##_stack), \
@@ -63,7 +39,7 @@ K_THREAD_STACK_DEFINE(uss_controller_stack, 2048);
 
 void reset_usb_hub()
 {
-    if (const device *gpioj{device_get_binding("gpio@40022400")}; device_is_ready(gpioj)) {
+    if (const device *gpioj{device_get_binding("GPIOJ")}; device_is_ready(gpioj)) {
         gpio_pin_configure(gpioj, 13, GPIO_OUTPUT_HIGH | GPIO_ACTIVE_HIGH);
         k_msleep(1);
         gpio_pin_set(gpioj, 13, 0);
@@ -74,41 +50,17 @@ void reset_usb_hub()
 
 }
 
-int main()
+void main()
 {
     reset_usb_hub();
-    lexxhard::actuator_controller::init();
-    lexxhard::adc_reader::init();
-    lexxhard::can_controller::init();
-    lexxhard::firmware_updater::init();
-    lexxhard::imu_controller::init();
-    lexxhard::interlock_controller::init();
-    lexxhard::led_controller::init();
-    lexxhard::misc_controller::init();
-    lexxhard::pgv_controller::init();
-    lexxhard::rosserial::init();
-    lexxhard::rosserial_service::init();
-    lexxhard::runaway_detector::init();
-    lexxhard::tof_controller::init();
-    lexxhard::uss_controller::init();
-    RUN(actuator_controller, 2);
-    RUN(adc_reader, 2);
-    RUN(can_controller, 4);
-    RUN(firmware_updater, 7);
-    RUN(imu_controller, 2);
-    RUN(interlock_controller, 5);
-    RUN(led_controller, 1);
-    RUN(misc_controller, 2);
-    RUN(pgv_controller, 1);
-    RUN(tof_controller, 2);
-    RUN(uss_controller, 2);
-    RUN(runaway_detector, 4);
-    RUN(rosserial, 5); // The rosserial thread will be started last.
-    RUN(rosserial_service, 6); // The rosserial thread will be started last.
-    const device *gpiog{device_get_binding("gpio@40021800")};
+    lexxhard::can_dummy_v7::init();
+    // lexxhard::can_dummy_v7_2::init();
+    const device *gpiog{device_get_binding("GPIOG")};
     if (gpiog != nullptr)
         gpio_pin_configure(gpiog, 7, GPIO_OUTPUT_LOW | GPIO_ACTIVE_HIGH);
     int heartbeat_led{1};
+    RUN(can_dummy_v7, 4);
+    // RUN(can_dummy_v7_2, 4);
     while (true) {
         if (gpiog != nullptr) {
             gpio_pin_set(gpiog, 7, heartbeat_led);
@@ -116,7 +68,6 @@ int main()
         }
         k_msleep(1000);
     }
-    return 0;
 }
 
 // vim: set expandtab shiftwidth=4:
