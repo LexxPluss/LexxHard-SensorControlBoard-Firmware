182d4363cd3d7c9e22628eb5ff3673b1d94523da
Atsunori Saito <sai@yedo.com>
2024-03-01 16:15:56 +0900

    feat: support to build with v3.6.0

--- lexxpluss_apps/src/main.cpp
+++ lexxpluss_apps/src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, LexxPluss Inc.
+ * Copyright (c) 2022-2024, LexxPluss Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,43 +23,10 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <zephyr.h>
-#include <drivers/gpio.h>
-#include "actuator_controller.hpp"
-#include "adc_reader.hpp"
-#include "can_controller.hpp"
-#include "firmware_updater.hpp"
-#include "imu_controller.hpp"
-#include "interlock_controller.hpp"
-#include "led_controller.hpp"
-#include "misc_controller.hpp"
-#include "pgv_controller.hpp"
-#include "rosserial.hpp"
-#include "rosserial_service.hpp"
-#include "runaway_detector.hpp"
-#include "tof_controller.hpp"
-#include "uss_controller.hpp"
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
 
 namespace {
-
-K_THREAD_STACK_DEFINE(actuator_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(adc_reader_stack, 2048);
-K_THREAD_STACK_DEFINE(can_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(firmware_updater_stack, 2048);
-K_THREAD_STACK_DEFINE(imu_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(interlock_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(led_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(misc_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(pgv_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(rosserial_stack, 2048);
-K_THREAD_STACK_DEFINE(rosserial_service_stack, 2048);
-K_THREAD_STACK_DEFINE(runaway_detector_stack, 2048);
-K_THREAD_STACK_DEFINE(tof_controller_stack, 2048);
-K_THREAD_STACK_DEFINE(uss_controller_stack, 2048);
-
-#define RUN(name, prio) \
-    k_thread_create(&lexxhard::name::thread, name##_stack, K_THREAD_STACK_SIZEOF(name##_stack), \
-                    lexxhard::name::run, nullptr, nullptr, nullptr, prio, K_FP_REGS, K_MSEC(2000));
 
 void reset_usb_hub()
 {
@@ -74,37 +41,9 @@ void reset_usb_hub()
 
 }
 
-void main()
+int main()
 {
     reset_usb_hub();
-    lexxhard::actuator_controller::init();
-    lexxhard::adc_reader::init();
-    lexxhard::can_controller::init();
-    lexxhard::firmware_updater::init();
-    lexxhard::imu_controller::init();
-    lexxhard::interlock_controller::init();
-    lexxhard::led_controller::init();
-    lexxhard::misc_controller::init();
-    lexxhard::pgv_controller::init();
-    lexxhard::rosserial::init();
-    lexxhard::rosserial_service::init();
-    lexxhard::runaway_detector::init();
-    lexxhard::tof_controller::init();
-    lexxhard::uss_controller::init();
-    RUN(actuator_controller, 2);
-    RUN(adc_reader, 2);
-    RUN(can_controller, 4);
-    RUN(firmware_updater, 7);
-    RUN(imu_controller, 2);
-    RUN(interlock_controller, 5);
-    RUN(led_controller, 1);
-    RUN(misc_controller, 2);
-    RUN(pgv_controller, 1);
-    RUN(tof_controller, 2);
-    RUN(uss_controller, 2);
-    RUN(runaway_detector, 4);
-    RUN(rosserial, 5); // The rosserial thread will be started last.
-    RUN(rosserial_service, 6); // The rosserial thread will be started last.
     const device *gpiog{device_get_binding("GPIOG")};
     if (gpiog != nullptr)
         gpio_pin_configure(gpiog, 7, GPIO_OUTPUT_LOW | GPIO_ACTIVE_HIGH);
@@ -116,6 +55,7 @@ void main()
         }
         k_msleep(1000);
     }
+    return 0;
 }
 
 // vim: set expandtab shiftwidth=4:
