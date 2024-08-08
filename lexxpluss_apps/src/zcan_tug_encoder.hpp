b32468a3ef3c6890bcafd7141f4f7aff36712960
Masahiro Wada <argon.argon.argon@gmail.com>
2024-08-08 22:37:57 +0900

    Update lexxpluss_apps/src/zcan_tug_encoder.hpp
    Co-authored-by: MIURA Yasuyuki <kokosabu@gmail.com>

--- lexxpluss_apps/src/zcan_tug_encoder.hpp
+++ lexxpluss_apps/src/zcan_tug_encoder.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, LexxPltug_encoder Inc.
+ * Copyright (c) 2024, LexxPltug_encoder Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
