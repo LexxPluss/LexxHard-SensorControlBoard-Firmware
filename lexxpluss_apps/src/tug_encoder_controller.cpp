319ed850eb8c63f856675198b17f62477faf189f
Masahiro Wada <masahiro.wada@lexxpluss.com>
2024-09-13 22:39:18 +0900

    HWDEV-2240 use RAW ANGLE to calibrate

--- lexxpluss_apps/src/tug_encoder_controller.cpp
+++ lexxpluss_apps/src/tug_encoder_controller.cpp
@@ -47,7 +47,6 @@ char __aligned(4) msgq_buffer[8 * sizeof (msg)];
 class tug_encoder_controller_impl {
 public:
     int init() {
-        k_mutex_init(&mut);
         k_msgq_init(&msgq, msgq_buffer, sizeof (msg), 8);
 
         dev = DEVICE_DT_GET(DT_NODELABEL(i2c2));
@@ -72,8 +71,7 @@ public:
         }
 
         while (true) {
-            auto const ret{fetch_encoder_value()};
-            if (!ret) {
+            if (!fetch_encoder_value()) {
                 LOG_ERR("Failed to fetch encoder value");
             }
 
@@ -85,19 +83,25 @@ public:
 
     void tug_encoder_info(const shell *shell) const {
         float const angle_deg{message.angle * 360.0f / 4096.0f};
-        int8_t const burn_count{get_burn_count().value_or(-1)};
+        int const burn_count{get_burn_count().value_or(-1)};
+        int const raw_angle{get_raw_angle().value_or(-1)};
+        int const zpos{get_zero_angle().value_or(-1)};
+        int const mpos{get_max_angle().value_or(-1)};
         shell_print(shell, "Angle: %f[deg]", angle_deg);
         shell_print(shell, "TUG connected: %d", is_tug_connected);
         shell_print(shell, "Magnet detected: %d", is_magnet_detected());
+        shell_print(shell, "Raw Angle: %d", raw_angle);
         shell_print(shell, "Burn count: %d", burn_count);
+        shell_print(shell, "ZPOS: %d", zpos);
+        shell_print(shell, "MPOS: %d", mpos);
 
-	if (burn_count == 0) {
+        if (burn_count == 0) {
             shell_print(shell, "");
             shell_print(shell, "***********************************************");
             shell_print(shell, " CAUTION!! This tug encoder is not caliblated. ");
             shell_print(shell, "***********************************************");
             shell_print(shell, "");
-	}
+        }
     }
 
     bool burn_angle()  {
@@ -111,20 +115,19 @@ public:
             return false;
         }
 
-        // burn angle
-        std::optional<uint16_t> const angle{get_angle()};
-        if (!angle.has_value()) {
-            LOG_ERR("Failed to get angle");
+	    auto const raw_angle{get_raw_angle()};
+        if (!raw_angle.has_value()) {
+            LOG_ERR("Failed to read raw angle");
             return false;
         }
 
-        if (!write_zpos(angle.value())) {
+        if (!write_zero_angle(raw_angle.value())) {
             LOG_ERR("Failed to write angle to AS5600_REG_ZPOS");
             return false;
         }
         k_msleep(3);
 
-        if (!write_mpos(angle.value())) {
+        if (!write_max_angle(raw_angle.value())) {
             LOG_ERR("Failed to write angle to AS5600_REG_MPOS");
             return false;
         }
@@ -152,34 +155,6 @@ private:
         return false;
     }
 
-    bool store_angle(uint16_t angle ) {
-        // message.angle is used as the bias angle to burn
-        // So message.angle is read by another thread (shell command)
-        // This means that message.angle should be updated atomically
-        if (k_mutex_lock(&mut, K_MSEC(100)) == 0) {
-            message.angle = angle;
-            k_mutex_unlock(&mut);
-        } else {
-            LOG_WRN("Cannot lock to store angle");
-            return false;
-        }
-
-        return true;
-    }
-
-    std::optional<uint16_t> get_angle() {
-        uint16_t angle;
-        if (k_mutex_lock(&mut, K_MSEC(100)) == 0) {
-            angle = message.angle;
-            k_mutex_unlock(&mut);
-        } else {
-            LOG_WRN("Cannot lock to read angle");
-            return std::nullopt;
-        }
-
-        return angle;
-    }
-
     bool fetch_encoder_value() {
         uint8_t angle_h;
         if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_ANGLE_H, &angle_h)) {
@@ -192,8 +167,8 @@ private:
             return false;
         }
 
-        auto const angle = (static_cast<uint16_t>(angle_h) << 8) | angle_l;
-        return store_angle(angle);
+        message.angle = (static_cast<uint16_t>(angle_h) << 8) | angle_l;
+        return true;
     }
 
     bool is_magnet_detected() const{
@@ -209,20 +184,68 @@ private:
     std::optional<uint8_t> get_burn_count() const {
         uint8_t burn_count;
         if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_ZMCO, &burn_count)) {
-            LOG_WRN("Failed to read burn count");
+            LOG_WRN("Failed to read ZMCO");
             return std::nullopt;
         }
 
         return burn_count;
     }
 
-    bool write_zpos(uint16_t angle) {
+    std::optional<uint16_t> get_zero_angle() const {
+        uint8_t angle_h;
+        if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_ZPOS_H, &angle_h)) {
+            LOG_WRN("Failed to read AS5600_REG_ZPOS_H");
+            return 0;
+        }
+
+        uint8_t angle_l;
+        if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_ZPOS_L, &angle_l)) {
+            LOG_WRN("Failed to read AS5600_REG_ZPOS_L");
+            return 0;
+        }
+
+        return (angle_h <<8) | angle_l;
+    }
+
+    std::optional<uint16_t> get_max_angle() const {
+        uint8_t angle_h;
+        if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_MPOS_H, &angle_h)) {
+            LOG_WRN("Failed to read AS5600_REG_MPOS_H");
+            return 0;
+        }
+
+        uint8_t angle_l;
+        if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_MPOS_L, &angle_l)) {
+            LOG_WRN("Failed to read AS5600_REG_MPOS_L");
+            return 0;
+        }
+
+        return (angle_h <<8) | angle_l;
+    }
+
+    std::optional<uint16_t> get_raw_angle() const {
+        uint8_t angle_h;
+        if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_RAW_ANGLE_H, &angle_h)) {
+            LOG_WRN("Failed to read AS5600_REG_RAW_ANGLE_H");
+            return 0;
+        }
+
+        uint8_t angle_l;
+        if (i2c_reg_read_byte(dev, AS5600_ADDR, AS5600_REG_RAW_ANGLE_L, &angle_l)) {
+            LOG_WRN("Failed to read AS5600_REG_RAW_ANGLE_L");
+            return 0;
+        }
+
+        return (angle_h <<8) | angle_l;
+    }
+
+    bool write_zero_angle(uint16_t angle) {
         uint8_t angle_h = (angle >> 8) & 0xFF;
-        uint8_t angle_l = angle & 0xFF;
         if (i2c_reg_write_byte(dev, AS5600_ADDR, AS5600_REG_ZPOS_H, angle_h)) {
             LOG_WRN("Failed to write angle to AS5600_REG_ZPOS_H");
             return false;
         }
+        uint8_t angle_l = angle & 0xFF;
         if (i2c_reg_write_byte(dev, AS5600_ADDR, AS5600_REG_ZPOS_L, angle_l)) {
             LOG_WRN("Failed to write angle to AS5600_REG_ZPOS_L");
             return false;
@@ -231,14 +254,14 @@ private:
         return true;
     }
 
-    bool write_mpos(uint16_t angle) {
+    bool write_max_angle(uint16_t angle) {
         uint8_t angle_h = (angle >> 8) & 0xFF;
-        uint8_t angle_l = angle & 0xFF;
-
         if (i2c_reg_write_byte(dev, AS5600_ADDR, AS5600_REG_MPOS_H, angle_h)) {
             LOG_WRN("Failed to write angle to AS5600_REG_MPOS_H");
             return false;
         }
+
+        uint8_t angle_l = angle & 0xFF;
         if (i2c_reg_write_byte(dev, AS5600_ADDR, AS5600_REG_MPOS_L, angle_l)) {
             LOG_WRN("Failed to write angle to AS5600_REG_MPOS_L");
             return false;
@@ -248,6 +271,12 @@ private:
     }
 
     bool write_burn_sequence() {
+        if (i2c_reg_write_byte(dev, AS5600_ADDR, AS5600_REG_BURN, 0x80)) {
+            LOG_WRN("Failed to write 0x01 to AS5600_REG_BURN");
+            return false;
+        }
+        k_msleep(5);
+
         if (i2c_reg_write_byte(dev, AS5600_ADDR, AS5600_REG_BURN, 0x01)) {
             LOG_WRN("Failed to write 0x01 to AS5600_REG_BURN");
             return false;
@@ -260,6 +289,7 @@ private:
             LOG_WRN("Failed to write 0x10 to AS5600_REG_BURN");
             return false;
         }
+        k_msleep(5);
 
         return true;
     }
@@ -267,7 +297,6 @@ private:
     msg message;
     const device *dev{nullptr};
     bool is_tug_connected{false};
-    struct k_mutex mut;
 
     static constexpr uint8_t AS5600_ADDR{0x36};
     static constexpr uint8_t AS5600_REG_ZMCO{0x00};
@@ -276,6 +305,8 @@ private:
     static constexpr uint8_t AS5600_REG_MPOS_H{0x03};
     static constexpr uint8_t AS5600_REG_MPOS_L{0x04};
     static constexpr uint8_t AS5600_REG_STATUS{0x0B};
+    static constexpr uint8_t AS5600_REG_RAW_ANGLE_H{0x0C};
+    static constexpr uint8_t AS5600_REG_RAW_ANGLE_L{0x0D};
     static constexpr uint8_t AS5600_REG_ANGLE_H{0x0E};
     static constexpr uint8_t AS5600_REG_ANGLE_L{0x0F};
     static constexpr uint8_t AS5600_REG_BURN{0xFF};
